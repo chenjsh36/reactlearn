47c8831c00de4acd00e74c9a3c9d588379a9759d
chenjsh36 <chenjsh36@outlook.com>
2016-02-19 14:09:50 +0800

    built a comment box follow the step

--- examples/2cjsfollow/react-tutorial-master/public/scripts/tutorial1.js
+++ examples/2cjsfollow/react-tutorial-master/public/scripts/tutorial1.js
@@ -11,23 +11,50 @@ var CommentBox = React.createClass({
 	getInitialState: function() {
 		return {data: []};
 	}, 
-	componentDidMount: function() {
+	loadCommentsFromServer: function() {
 		$.ajax({
 			url: this.props.url,
 			dataType: 'json',
 			cache: false,
 			success: function(data) {
+				this.setState({data: data})
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.log(this.props.rul, status, err.toString());
+			}.bind(this)
+		});
+	},
+	handleCommentSubmit: function(comment) {
+		var comments = this.state.data;
+		comment.id = Data.now();
+		var newComents = comments.concat([comment]);
+		this.setState({data: newComents});
+		// TODO: submit to the server and refresh the list
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			type: 'POST',
+			data: comment,
+			success: function(data) {
 				this.setState({data: data});
 			}.bind(this),
-			error//code here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			error: function(xhr, status, err) {
+				this.setState({data: comments});
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
 		})
+	},
+	// 在组件加载完毕的时候执行该函数
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
 	}, 
 	render: function() {
 		return (
 			<div className="commentBox">
 				<h1> Comments</h1>
 				<CommentList data={this.state.data}/>
-				<CommentForm/>
+				<CommentForm onCommentSubmit={this.handleCommentSubmit} />
 			</div>
 		);
 	}
@@ -37,7 +64,7 @@ var CommentBox = React.createClass({
 	初始入口ReactDOM.render()接口
  */
 ReactDOM.render(
-	<CommentBox url="/api/comments" />,
+	<CommentBox url="/api/comments" pollInterval={2000} />,
 	document.getElementById('content')
 );
 
